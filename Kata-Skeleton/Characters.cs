ac5c3146b1f2e558df98ce09e90d85c7ac8e6f45
VictorManas <vmanasmoya@cegid.com>
2022-04-29 13:37:13 +0200

    Parte Iteracion

---
+++ Kata-Skeleton/Characters.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Skeleton
+{
+    internal class Characters
+    {
+        public int Health { get; internal set; } = 1000;
+
+        internal bool IsAlive()
+        {
+            return true;
+        }
+
+        internal bool DealDamage(Characters rival)
+        {
+            //rival.Health - 50;
+            if (this.GetHashCode() == rival.GetHashCode())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        internal bool Heal(Characters rival)
+        {
+            if (this.GetHashCode() == rival.GetHashCode())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        internal bool CanBeHealed()
+        {
+
+            return (Health > 0) && (Health < 1000);     
+        }
+    }
+}
--- Kata-Skeleton/UnitTest1.cs
+++ Kata-Skeleton/UnitTest1.cs
@@ -1,15 +1,82 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Kata_Skeleton
 {
     [TestClass]
     public class UnitTest1
     {
+        //[TestMethod]
+        //public void CharacterStarter1Level()
+        //{
+        //    Assert.Inconclusive();
+        //}
+
         [TestMethod]
-        public void TestMethod1()
+        public void CharacterStarter1000health()
         {
-            Assert.AreEqual(200, 200);
+            Characters personaje = new Characters();
+            Assert.IsTrue(personaje.Health == 1000);
         }
+
+        [TestMethod]
+        public void isAlive()
+        {
+            Characters personaje = new Characters();
+            bool result = personaje.IsAlive();
+            Assert.IsTrue(result);
+        }
+
+
+
+
+
+        [TestMethod]
+        public void CharacterCanbeHealed()
+        {
+            Characters personaje = new Characters();
+            personaje.Health = 500;
+            Assert.IsTrue( personaje.CanBeHealed());
+
+        }
+        [TestMethod]
+        public void CharacterDealDamage()
+        {
+            Characters personaje = new Characters();
+            Characters rival = new Characters();
+            Assert.IsTrue(personaje.DealDamage(rival));
+        }
+        [TestMethod]
+        public void CharacterCantDamageHimself()
+        {
+            Characters personaje = new Characters();
+            
+            Assert.IsTrue(personaje.DealDamage(personaje));
+        }
+
+        [TestMethod]
+        public void CharacterHeal()
+        {
+            Characters personaje = new Characters();
+        
+            Assert.AreEqual(50, personaje.Heal(personaje));
+        }
+
+        [TestMethod]
+        public void CharacterCantHealhHimself()
+        {
+            //Characters personaje = new Characters();
+            //Characters rival = new Characters();
+            //Assert.AreEqual(50, personaje.Heal(rival));
+        }
+
+
+
+
     }
+
+
+
 }
